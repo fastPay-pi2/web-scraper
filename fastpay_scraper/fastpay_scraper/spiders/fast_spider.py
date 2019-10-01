ce05d6c3cf85ac2e4b549b6ced83ee76f32d2480
Rômulo Souza <romulovinicius10@gmail.com>
2019-09-30 23:58:05 -0300

    Changed spider to get category and subcategory

--- fastpay_scraper/fastpay_scraper/spiders/fast_spider.py
+++ fastpay_scraper/fastpay_scraper/spiders/fast_spider.py
@@ -7,37 +7,21 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.bigboxdelivery.com.br/cat/Alimentos-Basicos',
-            'https://www.bigboxdelivery.com.br/cat/Frios-e-Laticinios',
+            # 'https://www.bigboxdelivery.com.br/cat/Alimentos-Basicos',
             # 'https://www.bigboxdelivery.com.br/cat/Mercearia',
             # 'https://www.bigboxdelivery.com.br/cat/Bebidas-Nao-Alcoolicas',
+            # 'https://www.bigboxdelivery.com.br/cat/Bebidas',
+            # 'https://www.bigboxdelivery.com.br/cat/Condimentos-e-Molhos',
             # 'https://www.bigboxdelivery.com.br/cat/Biscoitos-e-Aperitivos',
-            # 'https://www.bigboxdelivery.com.br/cat/Limpeza'
-
+            # 'https://www.bigboxdelivery.com.br/cat/Doces',
+            # 'https://www.bigboxdelivery.com.br/cat/Higiene-e-Perfumaria',
+            'https://www.bigboxdelivery.com.br/cat/Limpeza'
         ]
         for url in urls:
             # print('url in start requests = ', url)
             yield SplashRequest(url=url, callback=self.parse)
 
     def parse(self, response):
-        items = response.css('single-item')
-        print('response = ', response.url)
-        for i in items:
-            yield {
-                'name': i.css('span.title::text').get(),
-                'brand': i.css('span.brand::text').get(),
-                'price': i.css('div.prices span.price::text').get(),
-                'image': i.css('a img.item-image').attrib['src']
-            }
-
-        # spans = response.css('span.title::text').getall()
-        # print('spans = ', spans)
-
-        # next_page = response.css('li a::attr(href)').get()
-        # print('#######################')
-        # print('next page = ', next_page)
-        # next_page = response.css('div.ib-pagination li a').attrib['href']
-
         pages = response.css('div.ib-pagination li')
 
         next_page = None
@@ -45,11 +29,28 @@ class QuotesSpider(scrapy.Spider):
             if i.css('span.next').get():
                 next_page = i.css('a').attrib['href']
 
-        # print('next page = ', next_page)
-
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield SplashRequest(url=next_page, callback=self.parse)
-        # else:
-        #     next_page = 'https://www.bigboxdelivery.com.br/cat/Alimentos-Basicos'
-        #     yield SplashRequest(url=next_page, callback=self.parse)
+
+        items = response.css('div.ib-single-item a')
+        for i in items:
+            next_page = response.urljoin(i.attrib['href'])
+            yield SplashRequest(url=next_page, callback=self.product_callback)
+
+
+    def product_callback(self, response):
+        category_path = response.css('div.ib-breadcrumb li')
+        category = category_path[1].css('a::text').get()
+        subcategory = category_path[2].css('a::text').get()
+
+        product_info = response.css('div.product-info')
+
+        for info in product_info:
+            yield {
+                'brand': info.css('h2.brand::text').get(),
+                'name': info.css('h1.name::text').get(),
+                'price': info.css('p.price span.price::text').get(),
+                'category': category,
+                'subcategory': subcategory
+            }
